2058dcaa5e5a5be8ecb8f2bae814b8dff9ee234c
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2021-10-03 15:34:21 -0300

    feat: theme defined

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+  colors: {
+    primary: "#5636D3",
+    secondary: "#FF872C",
+    secondaryligth: "rgba(255, 135, 44,.3)",
+
+    success: "#12A454",
+    successLight: "rgba(18, 164, 84,0.3)",
+
+    attention: "#E83F5B",
+    attentionLight: "rgba(232, 63, 91,.3)",
+
+    shape: "#fff",
+    title: "#363F5F",
+    text: "#969CB2",
+
+    background: "#F0F2F5",
+  },
+};
---
+++ src/screens/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => {
+  return <Container >
+    <Text>opa</Text>
+  </Container>;
+}
+
+export default Dashboard;
