5caf3c984df64631a58a11502293480b67293ff0
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2021-11-07 20:15:35 -0300

    chore:some changes

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { View } from "react-native";
 import Input from '../../components/form/Input'
+import { useAuth } from "../../hooks/useAuth";
 import { Container, FormContainer, LoginBtn,Logo,LoginButtonText } from "./styles";
 
 const Login: React.FC = () => {
+  const {signIn} = useAuth()
+  const {register,handleSubmit} = useForm()
+  const handleSignIn:SubmitHandler<any> = (data) => {
+    console.log(data);
+  }
   return (
     <Container>
-      <FormContainer>
+      <FormContainer onSubmit={handleSubmit(handleSignIn)}>
         <Logo source={require("../../assets/logo.png")}/>
         <Input placeholder='Email' autoComplete="email" autoCapitalize="none" />
         <Input placeholder='Senha' secureTextEntry />
